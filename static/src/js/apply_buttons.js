5aa2abe30bc11ff153b7120fdcff96b65233bea9
Rob Harrington <oeoeaio@gmail.com>
2018-05-21 20:16:18 +1000

    Add credit and debit buttons to account.move.item tree view

---
+++ static/src/js/apply_buttons.js
@@ -0,0 +1,29 @@
+odoo.define('pos.customer.account.apply_buttons', function (require) {
+    "use strict";
+    var core = require('web.core');
+    var ListController = require('web.ListController');
+
+    ListController.include({
+        renderButtons: function($node) {
+            this._super.apply(this, arguments);
+            if (this.$buttons) {
+                let credit_button = this.$buttons.find('.oe_apply_credit_button');
+                credit_button && credit_button.click(this.proxy('credit_button'));
+                let debit_button = this.$buttons.find('.oe_apply_debit_button');
+                debit_button && debit_button.click(this.proxy('debit_button'));
+            }
+        },
+        credit_button: function() {
+            this._callButtonAction({
+              type: 'action',
+              name: 'pos_customer_account.action_customer_account_apply_credit_form',
+            });
+        },
+        debit_button: function() {
+            this._callButtonAction({
+              type: 'action',
+              name: 'pos_customer_account.action_customer_account_apply_debit_form',
+            });
+        },
+    });
+})
