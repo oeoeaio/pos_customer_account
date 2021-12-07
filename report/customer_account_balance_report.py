e9fcd939673b3ea773e3624dcd5f02fe4d67de0e
Rob H <oeoeaio@gmail.com>
2021-12-08 09:08:07 +1100

    Update layout of customer account balance report

--- report/customer_account_balance_report.py
+++ report/customer_account_balance_report.py
@@ -1,10 +1,41 @@
 # -*- coding: utf-8 -*-
 
-import time
+import time, re
 from odoo import api, fields, models
 
 class CustomerAccountBalance(models.AbstractModel):
     _name = 'report.account.report_customer_account_balance'
+
+    def _account_payment(self, line):
+        match = re.search('^Account Payment$', line.name or '')
+        if match:
+            return 'Account Payment (at the hub)'
+
+    def _eft_payment(self, line):
+        match = re.search('^EFT-\d+-\d+$', line.name or '')
+        if match:
+            return 'EFT Payment ({ref})'.format(ref=line.ref)
+
+    def _stripe_payment(self, line):
+        ref_match = re.search('^OFN-STRIPE$', line.ref or '')
+        name_match = re.search('^R\d+$', line.name or '')
+        if ref_match and name_match:
+            return 'Card Payment (OFN - {ofn_order_no})'.format(ofn_order_no=line.name)
+
+    def _get_line_description(self, line):
+        if line['credit']:
+            credit_functions = [self._account_payment, self._eft_payment, self._stripe_payment]
+            for function in credit_functions:
+                result = function(line)
+                if result:
+                  return result
+            return 'Account Credit ({name} {ref})'.format(name=line.name, ref=line.ref)
+        else:
+            match = re.search('POS/\d{4}/\d{2}/\d{2}/(\d+)', line.ref or '')
+            if match:
+                return 'Account Purchase ({name} {order_no})'.format(name=line.name, order_no=match.group(1))
+            else:
+                return 'Account Purchase ({name} {ref})'.format(name=line.name, ref=line.ref)
 
     def _get_account_move_lines(self, customer_ids):
         res = {x: [] for x in customer_ids}
@@ -15,8 +46,7 @@ class CustomerAccountBalance(models.AbstractModel):
             for line in customer.account_lines.sorted('date'):
                 res[customer.id].append({
                     'date': line.date,
-                    'ref': line.ref,
-                    'name': line.name,
+                    'description': self._get_line_description(line),
                     'debit': line.debit,
                     'credit': line.credit,
                     'currency_id': line.currency_id,
@@ -57,12 +87,12 @@ class CustomerAccountBalance(models.AbstractModel):
 
             for currency in lines_to_display[partner_id]:
                 lines_to_display[partner_id][currency].reverse()
-                del lines_to_display[partner_id][currency][7:]
         return {
             'doc_ids': docids,
             'doc_model': 'res.partner',
             'docs': self.env['res.partner'].browse(docids),
             'time': time,
+            'currency': company_currency,
             'Lines': lines_to_display,
             'Totals': totals,
             'Date': fields.date.today(),
