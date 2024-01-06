289c3edd55942c61ba1764885ce0ea895e393993
Rob Harrington <oeoeaio@gmail.com>
2024-01-06 22:38:09 +1100

    Allow customer account debits to be included in bulk imports

--- wizard/bulk_import.py
+++ wizard/bulk_import.py
@@ -8,7 +8,8 @@ _logger = logging.getLogger(__name__)
 
 class BulkImport(models.TransientModel):
     _name = 'pos.customer.account.bulk_import'
-    _EXPECTED_FIELDS = ['date', 'customer_id', 'label', 'reference', 'credit']
+    _REQUIRED_FIELDS = ['date', 'customer_id', 'label', 'reference']
+    _OPTIONAL_FIELDS = ['credit', 'debit']
 
     data = fields.Binary(string="Upload File")
     file_name = fields.Char(string="File Name")
@@ -27,6 +28,7 @@ class BulkImport(models.TransientModel):
         for row in readCSV:
             transaction = self._parse(row)
             self._check_for_existing_move(transaction)
+            self._check_debit_or_credit(transaction)
             self._apply(transaction)
         return {}
 
@@ -36,7 +38,8 @@ class BulkImport(models.TransientModel):
           'partner_id': self._get_partner_id(row['customer_id']),
           'label': self._get_label(row['label']),
           'reference': self._get_reference(row['reference']),
-          'credit': self._get_credit(row['credit'])
+          'credit': self._get_credit(row.get('credit') or '0.0'),
+          'debit': self._get_debit(row.get('debit') or '0.0')
         }
 
     def _data_file(self):
@@ -45,9 +48,11 @@ class BulkImport(models.TransientModel):
         return io.StringIO(data_str)
 
     def _validate_headers(self, fieldnames):
-      if fieldnames != self._EXPECTED_FIELDS:
-          expected_fields = ', '.join(self._EXPECTED_FIELDS)
-          raise ValidationError("Unexpected field names found. Expected: %s." %(expected_fields))
+      all_required_fields_present = len(set(fieldnames) & set(self._REQUIRED_FIELDS)) == len(self._REQUIRED_FIELDS)
+      at_least_one_optional_field_present = len(set(fieldnames) & set(self._OPTIONAL_FIELDS)) > 0
+      if not (all_required_fields_present and at_least_one_optional_field_present):
+          expected_fields = ', '.join(self._REQUIRED_FIELDS)
+          raise ValidationError("Unexpected field names found. Expected all of: %s and at least one of %s." %(self._REQUIRED_FIELDS, self._OPTIONAL_FIELDS))
 
     def _get_date(self, raw_date):
         match = re.search('^\d{4}-\d{1,2}-\d{1,2}$', raw_date)
@@ -57,7 +62,7 @@ class BulkImport(models.TransientModel):
 
     def _get_partner_id(self, external_id):
         try:
-            partner = self.env['ir.model.data'].get_object('__import__', external_id)
+            partner = self.env.ref('__import__.%s' %(external_id))
             return partner.id
         except ValueError:
             raise ValidationError("Could not find customer: %s." %(external_id))
@@ -75,18 +80,27 @@ class BulkImport(models.TransientModel):
     def _get_credit(self, raw_credit):
         try:
             credit = float(raw_credit)
-            if credit <= 0:
-                raise ValidationError("Invalid credit amount found (%s). Credit must be a positive number." %(raw_credit))
+            if credit < 0:
+                raise ValidationError("Invalid credit amount found (%s). Credit must be a positive number (or zero for debits)." %(raw_credit))
             return credit
         except ValueError:
-            raise ValidationError("Invalid credit amount found (%s). Credit must be a positive number." %(raw_credit))
+            raise ValidationError("Invalid credit amount found (%s). Credit must be a positive number (or zero for debits)." %(raw_credit))
+
+    def _get_debit(self, raw_debit):
+        try:
+            debit = float(raw_debit)
+            if debit < 0:
+                raise ValidationError("Invalid debit amount found (%s). Debit must be a positive number (or zero for credits)." %(raw_debit))
+            return debit
+        except ValueError:
+            raise ValidationError("Invalid debit amount found (%s). Debit must be a positive number (or zero for credits)." %(raw_debit))
 
     @memoize
-    def _counterpart_account_id(self):
+    def _bank_account_account_id(self):
         return self.env.ref('l10n_au.1_au_11110').id
 
     @memoize
-    def _account_id(self):
+    def _trade_debtors_account_id(self):
         return self.env.ref('l10n_au.1_au_11200').id
 
     @memoize
@@ -98,23 +112,28 @@ class BulkImport(models.TransientModel):
         if result:
             raise ValidationError('Transaction %s already exists.' %(transaction['label']))
 
+    def _check_debit_or_credit(self, transaction):
+        if transaction['credit'] + transaction['debit'] == 0:
+            raise ValidationError('Each row must contain either a credit or debit amount.')
+
     def _apply(self, transaction):
         move = self._create_account_move(transaction['date'], transaction['reference'])
+        # create xmlid for move
         self.env['ir.model.data'].create({'name': transaction['label'], 'model': 'account.move', 'res_id': move.id})
 
         lines = [(0,0,{
             'name': transaction['label'],
             'move_id': move.id,
-            'account_id': self._account_id(),
+            'account_id': self._trade_debtors_account_id(),
             'partner_id': transaction['partner_id'],
             'credit': transaction['credit'],
-            'debit': 0.0,
+            'debit': transaction['debit'],
         }),(0,0,{
             'name': transaction['label'],
             'move_id': move.id,
-            'account_id': self._counterpart_account_id(),
+            'account_id': self._bank_account_account_id(),
             'partner_id': transaction['partner_id'],
-            'credit': 0.0,
+            'credit': transaction['debit'],
             'debit': transaction['credit'],
         })]
         _logger.info(str(lines))
